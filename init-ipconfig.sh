0fe0cb44d7f1229b74089fa3e0c4c8b6c359fd64
zagabond <jhj46456@gmail.com>
2021-07-10 00:54:45 +0900

    Update init-ipconfig.sh

--- init-ipconfig.sh
+++ init-ipconfig.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#----------------------
+# text color
+#----------------------
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+BG_RED="\e[1;41m"
+BG_GREEN="\e[1;42m"
+BG_YELLOW="\e[1;43m"
+BG_MAGENTA="\e[1;45m"
+BG_CYAN="\e[1;46m"
+NC="\e[0m"
+
 echo
 echo -e "${BG_GREEN} Installing firewall... ${NC}"
 sudo apt install iptables -y
