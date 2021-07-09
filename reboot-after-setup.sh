84d19033dced096a62401b03ae5a993acd2eabd1
zagabond <jhj46456@gmail.com>
2021-07-10 00:55:24 +0900

    Update reboot-after-setup.sh

--- reboot-after-setup.sh
+++ reboot-after-setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#----------------------
+# text color
+#----------------------
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+YELLOW="\e[1;33m"
+BG_RED="\e[1;41m"
+BG_GREEN="\e[1;42m"
+BG_YELLOW="\e[1;43m"
+BG_MAGENTA="\e[1;45m"
+BG_CYAN="\e[1;46m"
+NC="\e[0m"
 
 echo
 echo -e "${BG_YELLOW} Restart automatically... ${NC}"
