6a3eafe2d2d2f2fec8f849965bb4054abc915720
zagabond <jhj46456@gmail.com>
2021-07-10 00:38:17 +0900

    Create check-os.sh

---
+++ check-os.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#----------------------
+# check os
+#----------------------
+if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        OS=$NAME
+        VER=$VERSION_ID
+fi
+
+if [ "$OS" != "Ubuntu" ]; then
+        echo "This script seems to be running on an unsupported distribution."
+        echo "Supported distribution is Ubuntu."
+        exit
+fi
