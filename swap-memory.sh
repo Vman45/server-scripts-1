a2f9e40032cfa700a6a9303a78449fee3cc389d2
zagabond <jhj46456@gmail.com>
2021-07-10 00:56:50 +0900

    Update swap-memory.sh

--- swap-memory.sh
+++ swap-memory.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-#----------------------
-# text color
-#----------------------
-RED="\e[1;31m"
-GREEN="\e[1;32m"
-YELLOW="\e[1;33m"
-BG_RED="\e[1;41m"
-BG_GREEN="\e[1;42m"
-BG_YELLOW="\e[1;43m"
-BG_MAGENTA="\e[1;45m"
-BG_CYAN="\e[1;46m"
-NC="\e[0m"
-# ---------------------
-# swap memory
-# ---------------------
+
 echo
 echo -e "${BG_GREEN} Setting swap memory... ${NC}"
 sudo fallocate -l 1GB /swapfile
