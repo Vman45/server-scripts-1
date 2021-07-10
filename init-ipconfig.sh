6c748b79f73b3028e39ad915204a70b03dc2af1f
zagabond <jhj46456@gmail.com>
2021-07-11 08:26:25 +0900

    Update init-ipconfig.sh

--- init-ipconfig.sh
+++ init-ipconfig.sh
@@ -19,4 +19,4 @@ echo -e "${BG_GREEN} Installing firewall... ${NC}"
 sudo apt install iptables -y
 
 mkdir config
-sudo iptables-save > config/iptables.dump
+sudo iptables-save > $HOME/config/iptables.dump
